6ac00100e7c92056dd29e80a6cf28ce780abf405
Christabel Johnson <christabeljohnson33@gmail.com>
2025-03-30 09:25:47 +0100

    Project cards slide sideways but still issues

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
-const swiper = new Swiper(".slider-wrapper", {
-  loop: true,
-  grapCursor: true,
-  spaceBetween: 10,
-
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  //responsive breakpoints
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-      spaceBetween: 5,
-    },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 10,
-    },
-    1024: {
-      slidesPerView: 3,
-      spaceBetween: 10,
-    },
-  },
-});
-
 // project overlay
 document.addEventListener("DOMContentLoaded", function () {
   const menuButtons = document.querySelectorAll(".project-menu");
